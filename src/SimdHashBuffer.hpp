b673e52741a1aed7bc37cf5ea6fa4d918a322177
Gareth Evans <gazz.sf@gmil.com>
2024-06-23 12:55:00 +0100

    Add SimdHashBufferFixed

--- src/SimdHashBuffer.hpp
+++ src/SimdHashBuffer.hpp
@@ -29,14 +29,14 @@ public:
     }
     SimdHashBuffer(const size_t Width) :
         SimdHashBuffer(Width, SimdLanes()) {};
-    const size_t Width(void) const { return m_Width; };
-    const size_t Count(void) const { return m_Count; };
+    const size_t GetWidth(void) const { return m_Width; };
+    const size_t GetCount(void) const { return m_Count; };
     uint8_t* Buffer(void) { return &m_Buffer[0]; };
     uint8_t* Buffer(const size_t Index) { return m_BufferPointers[Index]; };
     uint8_t* operator[](const size_t Index) { return m_BufferPointers[Index]; };
     uint8_t** Buffers(void) { return &m_BufferPointers[0]; };
     const uint8_t** ConstBuffers(void) { return (const uint8_t**) &m_BufferPointers[0]; };
-    const size_t* Lengths(void) const { return &m_Lengths[0]; };
+    const size_t* GetLengths(void) const { return &m_Lengths[0]; };
     void SetLength(const size_t Index, const size_t Length) { m_Lengths[Index] = Length; };
     const size_t GetLength(const size_t Index) { return m_Lengths[Index]; };
 private:
@@ -45,6 +45,33 @@ private:
     std::vector<uint8_t> m_Buffer;
     std::vector<uint8_t*> m_BufferPointers;
     std::vector<size_t> m_Lengths;
+};
+
+template <size_t Width, size_t Count=MAX_LANES>
+class SimdHashBufferFixed
+{
+public:
+    SimdHashBufferFixed(void)
+    {
+        for (size_t i = 0; i < Count; i++)
+        {
+            m_BufferPointers[i] = &m_Buffer[i * Width];
+        }
+    }
+    const size_t GetWidth(void) const { return Width; };
+    const size_t GetCount(void) const { return Count; };
+    uint8_t* Buffer(void) { return &m_Buffer[0]; };
+    uint8_t* Buffer(const size_t Index) { return m_BufferPointers[Index]; };
+    uint8_t* operator[](const size_t Index) { return m_BufferPointers[Index]; };
+    uint8_t** Buffers(void) { return &m_BufferPointers[0]; };
+    const uint8_t** ConstBuffers(void) { return (const uint8_t**) &m_BufferPointers[0]; };
+    const size_t* GetLengths(void) const { return &m_Lengths[0]; };
+    void SetLength(const size_t Index, const size_t Length) { m_Lengths[Index] = Length; };
+    const size_t GetLength(const size_t Index) { return m_Lengths[Index]; };
+private:
+    std::array<uint8_t, Count * Width> m_Buffer;
+    std::array<size_t, Count> m_Lengths;
+    std::array<uint8_t*, Count> m_BufferPointers;
 };
 
 #endif // SimdHashBuffer_hpp
